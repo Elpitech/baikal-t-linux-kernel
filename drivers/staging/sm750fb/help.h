ad03ebf49abe2663946fa7777ed6ff2cdaadd476
Serge Semin <fancer.lancer@gmail.com>
2018-06-13 19:20:40 +0300

    video: sm750: Coding style refactoring
    This commit doesn't provide any functional fixup. Instead it just
    renames the files and alters the driver coding style so to comply
    with the kernel traditions.
    
    Original code is taken from BaikalSDK 4.14
    
    Signed-off-by: Serge Semin <fancer.lancer@gmail.com>

--- drivers/staging/sm750fb/help.h
+++ drivers/staging/sm750fb/help.h
@@ -1,12 +1,13 @@
-#ifndef DDK750_HELP_H__
-#define DDK750_HELP_H__
-#include "ddk750_chip.h"
+#ifndef HELP_H__
+#define HELP_H__
+
+#include "chip.h"
+
 #ifndef USE_INTERNAL_REGISTER_ACCESS
 
 #include <linux/ioport.h>
 #include <linux/io.h>
 #include <linux/uaccess.h>
-#include "sm750_help.h"
 
 /* software control endianness */
 #define PEEK32(addr) readl(addr + mmio750)
@@ -15,6 +16,7 @@
 extern void __iomem *mmio750;
 extern char revId750;
 extern unsigned short devId750;
+
 #else
 /* implement if you want use it*/
 #endif
