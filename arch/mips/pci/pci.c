8164e8e706a04223c7ffae0c3781efbf7b5353bb
Serge Semin <fancer.lancer@gmail.com>
2018-06-16 02:28:15 +0300

    MIPS: pci: Enable UCA on write_combine request
    Some of modern MIPS cores (P5600/6600, M5150/6520, end so on) can enable
    a special type Cache-Coherency attribute (CCA) named UnCached Accelerated
    attribute (UCA). We already make use of it in ioremap_wc() method by means
    of commit 9748e33e26c6 ("mips: mm: Create UCA-based ioremap_wc() method").
    The alteration only affects the kernel wc-dependent interface though, while
    there are methods which may require wc-capability bypassing the ioremap_wc()
    method. PCI bus proc (mmap callback) and sysfs (resourceX_wc attr for
    prefetchable resource) interfaces provide a way to map it' resources
    with write-combine attribute enabled. Lets permit this by handling
    the write_combine argument of pci_mmap_page_range() method so UCA attribute
    would be set at the vma page-protection of pages being mapped by the function.
    
    Signed-off-by: Vadim V. Vlasov <vadim.vlasov@t-platforms.ru>
    Signed-off-by: Serge Semin <fancer.lancer@gmail.com>

--- arch/mips/pci/pci.c
+++ arch/mips/pci/pci.c
@@ -61,8 +61,6 @@ void pci_resource_to_user(const struct pci_dev *dev, int bar,
 int pci_mmap_page_range(struct pci_dev *dev, struct vm_area_struct *vma,
 			enum pci_mmap_state mmap_state, int write_combine)
 {
-	unsigned long prot;
-
 	/*
 	 * I/O space can be accessed via normal processor loads and stores on
 	 * this platform but for now we elect not to do this and portable
@@ -71,12 +69,10 @@ int pci_mmap_page_range(struct pci_dev *dev, struct vm_area_struct *vma,
 	if (mmap_state == pci_mmap_io)
 		return -EINVAL;
 
-	/*
-	 * Ignore write-combine; for now only return uncached mappings.
-	 */
-	prot = pgprot_val(vma->vm_page_prot);
-	prot = (prot & ~_CACHE_MASK) | _CACHE_UNCACHED;
-	vma->vm_page_prot = __pgprot(prot);
+	if (write_combine)
+		vma->vm_page_prot = pgprot_writecombine(vma->vm_page_prot);
+	else
+		vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
 
 	return remap_pfn_range(vma, vma->vm_start, vma->vm_pgoff,
 		vma->vm_end - vma->vm_start, vma->vm_page_prot);
