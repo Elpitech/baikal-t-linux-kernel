59b850edb78da95d2013e67b0d1f0ff9356289bd
Serge Semin <fancer.lancer@gmail.com>
2018-09-04 13:29:25 +0300

    staging: sm750fb: Use Baikal eDMA instead of legacy Baikal DMA driver
    Since BaikalSDK 4.13 there is a eDMA driver supporting a separate PCIe
    DMA engine embedded into Baikal-T(1) SoC. This commit provides an
    alteration for sm750fb driver so one would use the eDAM driver instead of a
    weird legacy code Kostya fetched from someone. The purpose of the module
    is the same as before - entrust the frame-buffer copy process to
    eDMA engine instead of doing it by CPU.
    
    Signed-off-by: Vadim V. Vlasov <vadim.vlasov@t-platforms.ru>
    Signed-off-by: Serge Semin <fancer.lancer@gmail.com>

--- drivers/staging/sm750fb/sm750.c
+++ drivers/staging/sm750fb/sm750.c
@@ -1232,6 +1232,9 @@ static int lynxfb_pci_probe(struct pci_dev *pdev,
 	if (!sm750_dev)
 		return err;
 
+	if (smi_setup_dma(sm750_dev))
+		dev_err(&pdev->dev, "can't setup DMA!\n");
+
 	sm750_dev->fbinfo[0] = sm750_dev->fbinfo[1] = NULL;
 	sm750_dev->devid = pdev->device;
 	sm750_dev->revid = pdev->revision;
@@ -1287,6 +1290,7 @@ static int lynxfb_pci_probe(struct pci_dev *pdev,
 
 release_fb:
 	sm750fb_frambuffer_release(sm750_dev);
+	smi_release_dma(sm750_dev);
 	sm750_remove_ddc(sm750_dev);
 	return err;
 }
@@ -1298,6 +1302,7 @@ static void lynxfb_pci_remove(struct pci_dev *pdev)
 	sm750_dev = pci_get_drvdata(pdev);
 
 	sm750fb_frambuffer_release(sm750_dev);
+	smi_release_dma(sm750_dev);
 	sm750_remove_ddc(sm750_dev);
 	arch_phys_wc_del(sm750_dev->mtrr.vram);
 
